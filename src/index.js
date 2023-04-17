7267945e894968ccdd12bbdbd0fd7cbd6806dee9
Evan <eroberts1998@icloud.com>
2023-04-17 17:46:38 -0400

    complete besides the refresh issue

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     let movies; // declare movies variable outside function
     let currentMovie; // declare currentMovie variable outside function
   
-    const movieTitle = document.querySelector('#title');
-    const movieYear = document.querySelector('#year-released');
-    const movieDescription = document.querySelector('#description');
-    const movieWatched = document.querySelector('#watched');
-    const movieBloodAmount = document.querySelector('#amount');
-    const movieList = document.querySelector('#movie-list');
-    const bloodForm = document.querySelector('#blood-form');
+    const movieTitle = document.querySelector('#title'); // declare movieTitle variable outside function
+    const movieYear = document.querySelector('#year-released'); // declare movieYear variable outside function
+    const movieDescription = document.querySelector('#description'); // declare movieDescription variable outside function
+    const movieWatched = document.querySelector('#watched'); // declare movieWatched variable outside function
+    const movieBloodAmount = document.querySelector('#amount'); // declare movieBloodAmount variable outside function
+    const movieList = document.querySelector('#movie-list'); // declare movieList variable outside function
+    const bloodForm = document.querySelector('#blood-form'); // declare bloodForm variable outside function
   
     function displayMovieDetails(movie) {
       movieTitle.textContent = movie.title;
@@ -24,6 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   
+    
     function updateBloodAmount(movie, newBloodAmount) {
       fetch(`http://localhost:3000/movies/${movie.id}`, {
         method: 'PATCH',
